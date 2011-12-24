da4a533addfd45ca45b34dc0c7d609390cbb93dc
Al Wold <alwold@gmail.com>
2011-12-23 19:43:05 -0700

    rework this to pull all of my courses from the db for now

--- src/main/java/com/alwold/classwatch/service/ScheduleClassChecks.java
+++ src/main/java/com/alwold/classwatch/service/ScheduleClassChecks.java
@@ -2,10 +2,6 @@ package com.alwold.classwatch.service;
 
 import com.alwold.classwatch.dao.CourseDao;
 import com.alwold.classwatch.model.Course;
-import com.alwold.classwatch.model.School;
-import com.alwold.classwatch.model.Term;
-import com.alwold.classwatch.model.TermPk;
-import java.util.concurrent.LinkedBlockingQueue;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 
@@ -18,29 +14,23 @@ public class ScheduleClassChecks {
 	
 	@Autowired
 	private CourseDao courseDao;
+	@Autowired
 	private CourseQueue courseQueue;
 
 	public void setCourseQueue(CourseQueue courseQueue) {
 		this.courseQueue = courseQueue;
 	}
+
+	public void setCourseDao(CourseDao courseDao) {
+		this.courseDao = courseDao;
+	}
 	
 	public void scheduleClassChecks() throws InterruptedException {
 		logger.trace("scheduleClasschecks");
-		Course course = new Course();
-		course.setId(1L);
-		course.setCourseNumber("12345");
-		Term term = new Term();
-		TermPk termPk = new TermPk();
-		termPk.setCode("2127");
-		School school = new School();
-		school.setName("Arizona STate University");
-		school.setId(1L);
-		school.setPluginClass("com.alwold.classwatch.plugin.asu.AsuSchoolPlugin");
-		termPk.setSchool(school);
-		term.setPk(termPk);
-		course.setTerm(term);
-		logger.trace("put course");
-		courseQueue.put(course);
+		for (Course course: courseDao.getCourses("alwold@gmail.com")) {
+			logger.trace("put course");
+			courseQueue.put(course);
+		}
 	}
 	
 }
